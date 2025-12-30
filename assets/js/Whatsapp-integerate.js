8b7be96c5af2a9628dbfc84cbb9040a709cc2329
Laiba Khan <155344430+laiba-khan442@users.noreply.github.com>
2025-12-31 00:47:12 +0500

    implemented-dynamic-price-logic
    Used datasets in single-product.html to dynamically handle prices based on sizes, all of these can be changed, and are being fetched in the Whatsapp message correctly.

--- assets/js/Whatsapp-integerate.js
+++ assets/js/Whatsapp-integerate.js
@@ -1,31 +1,34 @@
 /*This file is used to generate a script to send user to Whatsapp when a user places an order*/
 
-let product = document.getElementById('product_name').innerText;
-let Sku = document.getElementById('product_SKU').innerText;
-let amount = document.getElementById('product_amount').innerText;
-let availability = document.getElementById('product_avail').innerText;
-let color = document.getElementById('product_color').innerText;
-let quantity = document.getElementById('product_quantity').innerText;
-let size = document.getElementById('product_size').value;
+//url formation
+function openWhatsApp() {
+  //message formation
+    let product = document.getElementById('product_name').innerText;
+    let Sku = document.getElementById('product_SKU').innerText;
+    let amount = document.getElementById('product_amount').innerText;
+    let availability = document.getElementById('product_avail').innerText;
+    let color = document.getElementById('product_color').innerText;
+    const qtyInput = document.querySelector("#product_quantity input.qty");
+    const quantity = parseInt(qtyInput.value, 10);
+    let size = document.getElementById('product_size_select').value;
 
-//message formation
 const message = 
-`Hi, I would like to place an order for\n` + 
-`Product: ${product}\n` +
-`SKU code: ${Sku}\n`+
-`Amount: ${amount}\n`+
-`Color: ${color}\n`+
-`Quantity: ${quantity}\n` +
-`Can I get more information on this?`; 
+
+    `Hi, I would like to place an order for\n` + 
+    `Product: ${product}\n` +
+    `SKU code: ${Sku}\n`+
+    `Amount: ${amount}\n` +
+    `Color: ${color}\n`+
+    `Quantity: ${quantity}\n` +
+    `Can I get more information on this?`; 
 
 const phone = "923372179541";
 
-//url formation
-function openWhatsApp() {
   const whatsappURL =
   `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(whatsappURL, "_blank");
-}
+};
 
+//Click button Event Listener
 document.getElementById('order')
   .addEventListener("click", openWhatsApp);
